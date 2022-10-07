3cdc18d1f9551025f8679f1bc411cde33768beaa
Rokibul Islam <mymainac2022@gmail.com>
2022-10-07 22:13:05 +0600

    Second commit

---
+++ Controllers/EmployeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc_Crud_Op.Data;
+using Mvc_Crud_Op.Models;
+
+namespace Mvc_Crud_Op.Controllers
+{
+    public class EmployeController : Controller
+    {
+        private readonly EmployeDbContext context;
+
+        public EmployeController(EmployeDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var result = context.Employe_Infos.ToList();
+
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+           return View();
+        }    
+        [HttpPost]
+        public IActionResult Create(Employe_Info employe)
+        {
+            if (ModelState.IsValid)
+            {
+                var employ = new Employe_Info()
+                {
+                    Name=employe.Name,
+                    Address=employe.Address,
+                    Mobile_No=employe.Mobile_No,
+                    Salary=employe.Salary,
+                   
+                };
+                context.Employe_Infos.Add(employ);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                TempData["Error"] = "Empty field Cannot Create";
+                return View();
+            }
+            
+        }
+    }
+}
--- Models/Employe_Info.cs
+++ Models/Employe_Info.cs
@@ -1,8 +1,11 @@
-﻿namespace Mvc_Crud_Op.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mvc_Crud_Op.Models
 {
     public class Employe_Info
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Address { get; set; }
         public string Mobile_No { get; set; }
